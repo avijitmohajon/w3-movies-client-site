fb40118fc92e90b2fe8d9e3cb01d372d7d1fea36
avijit mohajon <avijitmohajon72@gmail.com>
2025-05-15 14:43:21 +0600

    fix:spelling and spacing

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,36 +1,27 @@
-import React, { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import { InfinitySpin } from "react-loader-spinner";
-import { AuthContext } from '../AuthProvider/AuthProvider';
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const PrivateRoute = ({ children }) => {
+  const location = useLocation();
 
-const location = useLocation();
-
-
-    const { user, loading } = useContext(AuthContext);
-    if (loading) {
-  
-          <div className="flex items-center justify-center">
-            <InfinitySpin
-              visible={true}
-              width="200"
-              color="#4fa94d"
-              ariaLabel="infinity-spin-loading"
-            />
-          </div>
-       
-      }
-    if (user && user?.email) {
-        return children;
-    }
-
-    else  {
-
-        return <Navigate state={location.pathname} to="/login"></Navigate>
-    }
-
-
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    <div className="flex items-center justify-center">
+      <InfinitySpin
+        visible={true}
+        width="200"
+        color="#4fa94d"
+        ariaLabel="infinity-spin-loading"
+      />
+    </div>;
+  }
+  if (user && user?.email) {
+    return children;
+  } else {
+    return <Navigate state={location.pathname} to="/login"></Navigate>;
+  }
 };
 
 export default PrivateRoute;
